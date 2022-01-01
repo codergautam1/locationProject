b4613aa9d09993d29b8a18f48ae80312a3f1a64c
codergautam1 <codergauta1@gmail.com>
2022-01-01 17:26:38 +0530

    add res

--- app/src/main/java/com/example/locationproject/DemoApp.kt
+++ app/src/main/java/com/example/locationproject/DemoApp.kt
@@ -1,6 +1,21 @@
 package com.example.locationproject
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 /**
-* Created by King Coder On 01-01-2022
-**/class DemoApp {
+ * Created by King Coder On 01-01-2022
+ **/
+fun getLastSevenDate():String {
+    val formatter = SimpleDateFormat("yyyy-MM-dd")
+    return formatter.format(Date(System.currentTimeMillis() - 604800000L))
+}
+
+fun changeAadharNumber(aadharNumber:String):String {
+    return  aadharNumber.replace("\\w(?=\\w{4})".toRegex(), "*")
+}
+
+fun main() {
+    println(getLastSevenDate())
+    println(changeAadharNumber("1201313032112"))
 }
--- app/src/main/java/com/example/locationproject/HomeActivity.kt
+++ app/src/main/java/com/example/locationproject/HomeActivity.kt
@@ -1,7 +1,9 @@
 package com.example.locationproject
 
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -10,39 +12,79 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.locationproject.databinding.ActivityHomeBinding
+import com.google.android.gms.maps.model.Marker
 
 class HomeActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityHomeBinding
-
+    private lateinit var geoCoder: Geocoder
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        geoCoder = Geocoder(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+    private val mapDragListener = object:GoogleMap.OnMarkerDragListener {
+        override fun onMarkerDrag(p0: Marker) {
+        }
+
+        override fun onMarkerDragEnd(p0: Marker) {
+        }
+
+        override fun onMarkerDragStart(p0: Marker) {
+            val it = p0.position
+            try {
+                val addresses1 = geoCoder.getFromLocation(it.latitude,it.longitude,1)
+                if(addresses1.size > 0) {
+                    val addres = addresses1[0]
+                    val streetAddress = addres.getAddressLine(0)
+                    mMap.addMarker(MarkerOptions().position(it).title(streetAddress))
+                }
+            }catch(e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.setOnMapLongClickListener {
+            Log.i("TAG", "onMapReady: $it")
+            try {
+                val addresses1 = geoCoder.getFromLocation(it.latitude,it.longitude,1)
+                if(addresses1.size > 0) {
+                    val addres = addresses1.get(0)
+                    val streetAddress = addres.getAddressLine(0)
+                    mMap.addMarker(MarkerOptions().position(it).title(streetAddress).draggable(true))
+                }
+            }catch(e: Exception) {
+                e.printStackTrace()
+            }
+        }
+        mMap.setOnMarkerDragListener(mapDragListener)
+//        val latLong = LatLng(-14.0, 11.0)
+//        val markerOption =
+//            MarkerOptions().position(latLong).title("New Area Zone").snippet("Area 21 ")
+//        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLong, 16.0f)
+//        mMap.addMarker(markerOption)
+//        mMap.animateCamera(cameraUpdate)
+//        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Add Loction").snippet("Subtitle for Title"))
+//        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(sydney,16.0f))
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val address = geoCoder.getFromLocationName("noida",1)
+        val add = address[0]
+        val latLong = LatLng(add.latitude,add.longitude)
+        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLong,16.0f)
+        val marker = MarkerOptions().position(latLong).title(add.locality)
+        mMap.addMarker(marker)
+        mMap.moveCamera(cameraUpdate)
+
     }
 }
